24cd4fde2b33c01bf21d4c0bd922873a1ed8a37e
pranavliag <pranavmechary@gmail.com>
2019-04-03 18:48:18 +0530

    Feature popover arrow indicator and docs updated

--- tests/dummy/app/docs/core/pop-over/controller.ts
+++ tests/dummy/app/docs/core/pop-over/controller.ts
@@ -1,9 +1,43 @@
 import Controller from '@ember/controller';
+import { action } from '@ember-decorators/object';
 
-export default class DocsCorePopOver extends Controller.extend({
-  // anything which *must* be merged to prototype here
-}) {
+export default class DocsCorePopOver extends Controller {
   // normal class body definition here
+  placement: string = 'auto';
+  arrow: boolean = true;
+  canOutsideClickClose: boolean = true;
+  canEscapeKeyClose:boolean=true;
+  open:boolean=false;
+  @action
+  OnMouseEnter() {
+    var doc = document.querySelector('.docs-popover-example-scroll');
+    if (doc) {
+      var scrolldiv: any = document.querySelector('#pop-over-doc-scroll');
+      scrolldiv.scrollTop = doc.getBoundingClientRect().height / 4;
+      scrolldiv.scrollLeft = doc.getBoundingClientRect().width / 4;
+    }
+  }
+  @action
+  selectPositon(e: any) {
+    this.set('placement', e.target.value);
+  }
+  @action
+  oncanOutsideClickClose() {
+    this.set('canOutsideClickClose', !this.canOutsideClickClose);
+  }
+  @action
+  oncanEscapeKeyClose() {
+    this.set('canEscapeKeyClose', !this.canEscapeKeyClose);
+  }
+  @action
+  onArrow() {
+    this.set('arrow', !this.arrow);
+  }
+  @action
+  onClose(){
+    this.set('open',false);
+  }
+  
 }
 
 // DO NOT DELETE: this is how TypeScript knows how to look up your controllers.
