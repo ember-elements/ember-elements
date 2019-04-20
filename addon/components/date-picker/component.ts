c377f16d9b72b61c4ebb08fb38d3b753e99d20e5
pranavliag <pranavmechary@gmail.com>
2019-04-20 10:48:16 +0530

    BugFix InputGroupProps option for date picker input props

--- addon/components/date-picker/component.ts
+++ addon/components/date-picker/component.ts
@@ -5,7 +5,7 @@ import Ember from 'ember';
 import { action, computed } from '@ember-decorators/object';
 import { get, set } from '@ember/object';
 import { classNames, tagName, attribute, layout, className } from '@ember-decorators/component';
-import { readOnly } from '@ember-decorators/object/computed';
+import { readOnly, alias } from '@ember-decorators/object/computed';
 import * as Classes from '../../-private/common/classes';
 @layout(template)
 @tagName('span')
@@ -16,6 +16,29 @@ export default class DatePicker extends Component {
   @readOnly('open') Open?: boolean;
 
   @readOnly('format') Format!: string;
+
+  @alias('InputGroupProps.class') IGCLASS?: string;
+
+  @alias('InputGroupProps.style') IGSTYLE?: string;
+
+  @alias('InputGroupProps.disabled') IGDISABLED?: boolean;
+
+  @alias('InputGroupProps.intent') IGINTENT?: string;
+
+  @alias('InputGroupProps.large') IGLARGE?: boolean;
+
+  @alias('InputGroupProps.leftIcon') IGLEFTICON?: string;
+
+  @alias('InputGroupProps.rightIcon') IGRIGHTICON?: string;
+
+  @alias('InputGroupProps.iconSize') IGICONSIZE?: number;
+
+  @alias('InputGroupProps.round') IGROUND?: boolean;
+
+  @alias('InputGroupProps.small') IGSMALL?: boolean;
+
+  @alias('InputGroupProps.placeholder') IGPLACEHOLDER?: string;
+
 
   @className(Classes.POPOVER_OPEN)
   open: boolean = false;
