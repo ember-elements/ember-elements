393a3e2bca6a76d20d770098e964517bfcee1c4c
pranavliag <pranavmechary@gmail.com>
2019-04-24 22:10:33 +0530

    BugFix fixed lint error in InputGroup

--- tests/integration/components/input-group/component-test.ts
+++ tests/integration/components/input-group/component-test.ts
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, triggerKeyEvent, click, doubleClick, blur } from '@ember/test-helpers';
+import { render, triggerKeyEvent, click, doubleClick } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | input-group', function (hooks) {
