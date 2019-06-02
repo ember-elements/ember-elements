42f292b2220566add2c39efd36e52185bb0187c3
pranavliag <pranavmechary@gmail.com>
2019-06-02 09:40:51 +0530

    BugFix update ember-lts-3.8 devDependencies to 3.8.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -15,7 +15,7 @@ module.exports = function () {
           name: 'ember-lts-3.8',
           npm: {
             devDependencies: {
-              'ember-source': '~2.18.0'
+              'ember-source': '~3.8.0'
             }
           }
         },
