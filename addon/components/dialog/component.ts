08e16af991683b65cea3b4b4653d398e67d3a3a8
pranavliag <pranavmechary@gmail.com>
2019-06-20 11:39:00 +0530

    Feature Dialog component

--- addon/components/dialog/component.ts
+++ addon/components/dialog/component.ts
@@ -1,86 +1,97 @@
 import Component from '@ember/component';
 // @ts-ignore: Ignore import of compiled template
-import template from './template';
-import { action } from '@ember-decorators/object';
-import { readOnly } from '@ember-decorators/object/computed';
-import { layout } from '@ember-decorators/component';
+import layout from './template';
+import { action, computed } from '@ember-decorators/object';
+import { htmlSafe } from '@ember/string';
+import { get } from '@ember/object';
+
+import * as Errors from "../../-private/common/errors";
 import * as Classes from '../../-private/common/classes';
-@layout(template)
 export default class Dialog extends Component {
-  @readOnly('width') Width!: string;
+  layout = layout;
 
-  @readOnly('height') Height!: string;
+  backdropProps?: object = {};
 
-  @readOnly('marginTop') Top!: string;
+  className?: string;
 
-  @readOnly('marginLeft') Left!: string;
+  dialogClassName?: string;
 
-  @readOnly('marginRight') Right!: string;
+  enforceFocus?: boolean = true;
 
-  OVERLAY: string = Classes.OVERLAY;
-  OVERLAY_OPEN: string = Classes.OVERLAY_OPEN;
-  OVERLAY_SCROLL_CONTAINER: string = Classes.OVERLAY_SCROLL_CONTAINER;
-  OVERLAY_BACKDROP: string = Classes.OVERLAY_BACKDROP;
-  OVERLAY_CONTENT: string = Classes.OVERLAY_CONTENT;
-  DIALOG: string = Classes.DIALOG;
-  DIALOG_CONTAINER: string = Classes.DIALOG_CONTAINER;
-  isOutClickCloseDialog!: boolean;
-  isOpenDialog!: boolean;
-  ESC: number = 27;
-  dbId!: string;
-  isEscapeToClose!: boolean;
+  lazy?: boolean = true;
 
-  init() {
-    super.init();
-    this._closeOnEsc = this._closeOnEsc.bind(this);
-    this.enforceFocusFun = this.enforceFocusFun.bind(this);
+  isOpen?: boolean;
+
+  hasBackdrop?: boolean;
+
+  icon?: string;
+
+  /**
+    * Whether to show the close button in the dialog's header.
+    * Note that the header will only be rendered if `title` is provided.
+    * @default true
+    */
+  isCloseButtonShown: boolean = true;
+
+  style?: any;
+
+  title?: string;
+
+  transitionName: string = Classes.OVERLAY;
+
+  canEscapeKeyClose?: boolean = true;
+
+  canOutsideClickClose?: boolean = true;
+
+  usePortal: boolean = true;
+
+  portalClassName?: string;
+
+  onClose!: (event: any) => void;
+
+  @computed('style')
+  get dialogStyle() {
+    return htmlSafe(this.style);
   }
 
-  async didReceiveAttrs() {
-    this.set('dbId', 'dialog_box_id_' + this.elementId);
-    await this.set('isOpenDialog', this.get('isOpenDialog'));
-    var doc: any = await document.querySelector('#dialog_box_id_' + this.elementId);
-    if (this.isOpenDialog) {
-      doc.style.width = this.Width;
-      doc.style.height = this.Height;
-      doc.style.marginTop = this.Top;
-      doc.style.marginLeft = this.Left;
-      doc.style.marginRight = this.Right;
-    }
+  @computed('className')
+  get overlayClassName() {
+    return `${this.OVERLAY_SCROLL_CONTAINER} ${this.className}`;
   }
 
-  didRender() {
-    if (this.isOpenDialog) {
-      var focus: any = document.getElementById(this.dbId);
-      if (focus) {
-        focus.focus();
-        document.addEventListener('focus', this.enforceFocusFun, true);
-      }
-      if (this.get('isEscapeToClose'))
-        window.addEventListener('keyup', this._closeOnEsc);
-    }
-    else {
-      window.removeEventListener('keyup', this._closeOnEsc);
-      document.removeEventListener('focus', this.enforceFocusFun, true);
-    }
+  @computed('dialogClassName')
+  get dialogClassNames() {
+    return `${this.DIALOG} ${this.dialogClassName}`;
   }
+
+  @computed('portalClassName', 'usePortal')
+  get setPortalClassName() {
+    return `${this.usePortal ? this.PORTAL : ""} ${this.portalClassName}`;
+  }
+
+  DIALOG = Classes.DIALOG;
+  DIALOG_CONTAINER = Classes.DIALOG_CONTAINER;
+  DIALOG_CLOSE_BUTTON = Classes.DIALOG_CLOSE_BUTTON;
+  DIALOG_HEADER = Classes.DIALOG_HEADER;
+  HEADING = Classes.HEADING;
+  OVERLAY_SCROLL_CONTAINER = Classes.OVERLAY_SCROLL_CONTAINER;
+  PORTAL = Classes.PORTAL;
+  containerClassName = Classes.DIALOG_CONTAINER;
 
   @action
-  outerClickCloseDialog() {
-    if (this.isOutClickCloseDialog)
-      this.set('isOpenDialog', false);
+  onIconClick(e: any) {
+    if (get(this, 'onClose'))
+      get(this, 'onClose')(e)
   }
 
-  _closeOnEsc(e: any) {
-    if (e.keyCode === this.ESC) { this.set('isOpenDialog', false); }
-  }
-
-  enforceFocusFun(e: any) {
-    var focus: any = document.getElementById(this.dbId);
-    if (focus != null && !focus.contains(e.target)) {
-      focus.focus();
-      e.preventDefault();
-      e.stopImmediatePropagation();
+  protected validateProps() {
+    if (this.title == null) {
+      if (this.icon != null) {
+        console.warn(Errors.DIALOG_WARN_NO_HEADER_ICON);
+      }
+      if (this.isCloseButtonShown != null) {
+        console.warn(Errors.DIALOG_WARN_NO_HEADER_CLOSE_BUTTON);
+      }
     }
   }
   // normal class body definition here
--- tests/integration/components/dialog/component-test.ts
+++ tests/integration/components/dialog/component-test.ts
@@ -1,67 +1,204 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, triggerKeyEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | dialog', function (hooks) {
   setupRenderingTest(hooks);
 
-  // test('it renders', async function (assert) {
-  //   // Set any properties with this.set('myProperty', 'value');
-  //   // Handle any actions with this.set('myAction', function(val) { ... });
-
-  //   await render(hbs`{{dialog}}`);
-  //   var element:any=this.element;
-  //   assert.equal(element.textContent.trim(), '');
-
-  //   // Template block usage:
-  //   await render(hbs`
-  //     {{#dialog}}
-  //       template block text
-  //     {{/dialog}}
-  //   `);
-  //   var element:any=this.element;
-  //   assert.equal(element.textContent.trim(), 'template block text');
-  // });
   test('dialog rendering', async function (assert) {
-    await render(hbs` <Dialog @isOpenDialog=true>
+    await render(hbs` <Dialog @isOpen=true>
   </Dialog> <div id="destination"></div>`);
     console.log(document.querySelector('.bp3-dialog'))
     assert.equal(document.querySelectorAll('.bp3-dialog').length, 1);
   });
-  test('dialog height', async function (assert) {
-    await render(hbs` <Dialog @isOpenDialog=true @height='100px'/>
-   <div id="destination"></div>`);
-    var height: any = document.querySelector('.bp3-dialog');
-    assert.equal(height.style.height, '100px');
-    // document.querySelector('.bp3-dialog')
-  });
-  test('dialog width', async function (assert) {
-    await render(hbs` <Dialog @isOpenDialog=true @width='100px'>
+
+  test('renders its content correctly', async function (assert) {
+    await render(hbs` <Dialog @isOpen=true>
+                <div class="bp3-dialog-header" >
+                <Icon @icon="inbox" @iconSize=20 />
+                <h4>Dialog header</h4>
+            </div>
+            <div class="bp3-dialog-body" >
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna alqua. Ut enim ad minimum veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </p>
+            </div>
+            <div class="bp3-dailog-footer" >
+                <div class="bp3-dialog-footer-actions">
+                    <Button >Secondary</Button>
+                    <Button @intent="primary" @type="submit"  >Primary</Button>
+                </div>
+            </div>
   </Dialog> <div id="destination"></div>`);
-    var width: any = document.querySelector('.bp3-dialog');
-    assert.equal(width.style.width, '100px');
-    // document.querySelector('.bp3-dialog')
+    assert.equal(document.querySelectorAll('.bp3-dialog').length, 1);
+    assert.equal(document.querySelectorAll('.bp3-dialog-body').length, 1);
+    assert.equal(document.querySelectorAll('.bp3-dailog-footer').length, 1);
+    assert.equal(document.querySelectorAll('.bp3-dialog-footer-actions').length, 1);
+    assert.equal(document.querySelectorAll('.bp3-dialog-header').length, 1);
+    assert.equal(document.querySelectorAll('.bp3-overlay-backdrop').length, 1);
   });
-  test('dialog marginTop', async function (assert) {
-    await render(hbs` <Dialog @isOpenDialog=true @marginTop='100px'>
+
+  test('portalClassName appears on Portal', async function (assert) {
+    await render(hbs` <Dialog @isOpen=true @portalClassName="test-class">
+                <div class="bp3-dialog-header" key=0>
+                <Icon @icon="inbox" @iconSize=20 />
+                <h4>Dialog header</h4>
+            </div>,
+            <div class="bp3-dialog-body" key=1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna alqua. Ut enim ad minimum veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </p>
+            </div>,
+            <div class="bp3-dailog-footer" key=2>
+                <div class="bp3-dialog-footer-actions">
+                    <Button >Secondary</Button>
+                    <Button @intent="primary" @type="submit"  >Primary</Button>
+                </div>
+            </div>
   </Dialog> <div id="destination"></div>`);
-    var marginTop: any = document.querySelector('.bp3-dialog');
-    assert.equal(marginTop.style.marginTop, '100px');
-    // document.querySelector('.bp3-dialog')
+    assert.ok(document.querySelector(`.bp3-portal.test-class`));
   });
-  test('dialog marginLeft', async function (assert) {
-    await render(hbs` <Dialog @isOpenDialog=true @marginLeft='100px'>
+
+  test('attempts to close when overlay backdrop element is moused down', async function (assert) {
+    let that = this;
+    this.set('isOpen', true);
+    this.set("onClose", function () {
+      that.set('isOpen', false);
+    })
+    await render(hbs` <Dialog @isOpen={{isOpen}} @portalClassName="test-class" @onClose={{action onClose}}>
+                <div class="bp3-dialog-header" key=0>
+                <Icon @icon="inbox" @iconSize=20 />
+                <h4>Dialog header</h4>
+            </div>,
+            <div class="bp3-dialog-body" key=1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna alqua. Ut enim ad minimum veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </p>
+            </div>,
+            <div class="bp3-dailog-footer" key=2>
+                <div class="bp3-dialog-footer-actions">
+                    <Button >Secondary</Button>
+                    <Button @intent="primary" @type="submit"  >Primary</Button>
+                </div>
+            </div>
   </Dialog> <div id="destination"></div>`);
-    var marginLeft: any = document.querySelector('.bp3-dialog');
-    assert.equal(marginLeft.style.marginLeft, '100px');
-    // document.querySelector('.bp3-dialog')
+    await click('.bp3-overlay-backdrop');
+
+    assert.equal(document.querySelectorAll('.bp3-dialog').length, 0);
+
   });
-  test('dialog marginRight', async function (assert) {
-    await render(hbs` <Dialog @isOpenDialog=true @marginRight='100px'>
+
+  test('does not close when canOutsideClickClose = false and overlay backdrop element is moused down', async function (assert) {
+    let that = this;
+    this.set('isOpen', true);
+    this.set("onClose", function () {
+      that.set('isOpen', false);
+    })
+    await render(hbs` <Dialog @isOpen={{isOpen}} @canOutsideClickClose={{false}} @usePortal={{false}} @onClose={{action onClose}}>
+                <div class="bp3-dialog-header" key=0>
+                <Icon @icon="inbox" @iconSize=20 />
+                <h4>Dialog header</h4>
+            </div>,
+            <div class="bp3-dialog-body" key=1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna alqua. Ut enim ad minimum veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </p>
+            </div>,
+            <div class="bp3-dailog-footer" key=2>
+                <div class="bp3-dialog-footer-actions">
+                    <Button >Secondary</Button>
+                    <Button @intent="primary" @type="submit"  >Primary</Button>
+                </div>
+            </div>
   </Dialog> <div id="destination"></div>`);
-    var marginRight: any = document.querySelector('.bp3-dialog');
-    assert.equal(marginRight.style.marginRight, '100px');
-    // document.querySelector('.bp3-dialog')
+    await click('.bp3-overlay-backdrop');
+
+    assert.equal(document.querySelectorAll('.bp3-dialog').length, 1);
+
   });
+
+  test('does not close when canEscapeKeyClose = false and escape key is pressed', async function (assert) {
+    let that = this;
+    this.set('isOpen', true);
+    this.set("onClose", function () {
+      that.set('isOpen', false);
+    })
+    await render(hbs` <Dialog @isOpen={{isOpen}} @canEscapeKeyClose={{false}} @usePortal={{false}} @onClose={{action onClose}}>
+                <div class="bp3-dialog-header" key=0>
+                <Icon @icon="inbox" @iconSize=20 />
+                <h4>Dialog header</h4>
+            </div>,
+            <div class="bp3-dialog-body" key=1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna alqua. Ut enim ad minimum veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </p>
+            </div>,
+            <div class="bp3-dailog-footer" key=2>
+                <div class="bp3-dialog-footer-actions">
+                    <Button >Secondary</Button>
+                    <Button @intent="primary" @type="submit"  >Primary</Button>
+                </div>
+            </div>
+  </Dialog> <div id="destination"></div>`);
+    await triggerKeyEvent('.bp3-overlay-backdrop', 'keydown', 27);
+
+    assert.equal(document.querySelectorAll('.bp3-dialog').length, 1);
+
+  });
+
+  test('header  if title prop is given', async function (assert) {
+    await render(hbs` <Dialog @isOpen=true @title="Hello!" @icon="inbox">
+  </Dialog> <div id="destination"></div>`);
+    assert.ok(document.querySelector('.bp3-dialog-header'));
+  });
+
+
+  test('renders close button if isCloseButtonShown=true', async function (assert) {
+    await render(hbs` <Dialog @isOpen=true @isCloseButtonShown=true @title="Hello!" @icon="inbox">
+  </Dialog> <div id="destination"></div>`);
+    assert.ok(document.querySelector('svg'));
+  });
+
+  test('clicking close button triggers onClose', async function (assert) {
+    let that = this;
+    this.set('isOpen', true);
+    this.set("onClose", function () {
+      that.set('isOpen', false);
+    })
+    await render(hbs` <Dialog @isOpen={{isOpen}} @isCloseButtonShown={{true}} @usePortal={{false}} @onClose={{action onClose}}>
+                <div class="bp3-dialog-header" key=0>
+                <Icon @icon="inbox" @iconSize=20 />
+                <h4>Dialog header</h4>
+            </div>,
+            <div class="bp3-dialog-body" key=1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna alqua. Ut enim ad minimum veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </p>
+            </div>,
+            <div class="bp3-dailog-footer" key=2>
+                <div class="bp3-dialog-footer-actions">
+                    <Button >Secondary</Button>
+                    <Button @intent="primary" @type="submit"  >Primary</Button>
+                </div>
+            </div>
+  </Dialog> <div id="destination"></div>`);
+    await click('button');
+
+    assert.equal(document.querySelectorAll('.bp3-dialog').length, 1);
+
+  });
+
 });
