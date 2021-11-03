bb431f7b73ab8cf4f11d93b17b91d638d223b7da
pranavliag <pranavmechary@gmail.com>
2021-11-04 00:05:55 +0530

    docs(button): add stories for button component

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/components/core/**/*.stories.js'],
+  // stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
