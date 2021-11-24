d27910fe7ab4811f348fbc27b38f3077e493009b
pranavliag <pranavmechary@gmail.com>
2021-11-24 23:35:57 +0530

    docs(progressbar): add stories for progressbar component

--- packages/core/.storybook/preview.js
+++ packages/core/.storybook/preview.js
@@ -1,22 +1,22 @@
 export const parameters = {
-  layout: 'centered',
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	// layout: 'centered',
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
 };
 
 export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: 'light',
-    toolbar: {
-      icon: 'circlehollow',
-      items: ['light', 'dark'],
-    },
-  },
+	theme: {
+		name: 'Theme',
+		description: 'Global theme for components',
+		defaultValue: 'light',
+		toolbar: {
+			icon: 'circlehollow',
+			items: ['light', 'dark'],
+		},
+	},
 };
