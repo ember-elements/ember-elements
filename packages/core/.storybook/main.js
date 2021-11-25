459855615dbe25c9f1599e83bcfb3545eb3613df
pranavliag <pranavmechary@gmail.com>
2021-11-25 22:36:41 +0530

    docs(tags): add stories for tags component

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../stories/components/core/**/*.stories.js'],
-  // stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	stories: ['../stories/components/core/**/*.stories.js'],
+	// stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-essentials'],
 };
