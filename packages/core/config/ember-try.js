0e8f3b29419a2a7dde0334ae697b683c926a785d
LowBP <pranavmechary@gmail.com>
2023-08-15 22:24:57 +0200

    ci: update ember try

--- packages/core/config/ember-try.js
+++ packages/core/config/ember-try.js
@@ -15,10 +15,10 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-lts-3.20',
+        name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            'ember-source': '~3.20.5',
+            'ember-source': '~3.24.0',
           },
         },
       },
