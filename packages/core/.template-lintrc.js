bfbcd536bf55f536df01df4ce7e7cdd45a146828
LowBP <pranavmechary@gmail.com>
2023-08-24 13:39:51 +0200

    feat(core): update lint files

--- packages/core/.template-lintrc.js
+++ packages/core/.template-lintrc.js
@@ -5,6 +5,11 @@ module.exports = {
     quotes: 'single',
     'no-inline-styles': false,
     'no-implicit-this': true,
+    'no-pointer-down-event-binding': false,
+    'no-action': false,
+    'require-input-label': false,
+    'no-empty-headings': false,
+    'no-autofocus-attribute': false,
     'no-invalid-interactive': {
       additionalInteractiveTags: ['a'],
     },
